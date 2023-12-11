6d2471fcf84bf1314b1c1204ed8ffe2e36acbd77
kangsihwon <kangsihwon@gmail.com>
2023-12-12 07:13:44 +0900

    Jenkins Github Webhook Test2
    Test

--- ThirdPerson/Source/ThirdPerson/ThirdPersonCharacter.cpp
+++ ThirdPerson/Source/ThirdPerson/ThirdPersonCharacter.cpp
@@ -56,7 +56,7 @@ void AThirdPersonCharacter::BeginPlay()
 	// Call the base class  
 	Super::BeginPlay();
 
-	UE_LOG(LogTemp, Log, TEXT("Test"));
+	//UE_LOG(LogTemp, Log, TEXT("Test"));
 
 	//Add Input Mapping Context
 	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
